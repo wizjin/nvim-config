04fda1b2ff77e51f6479d7f3cb41f00c8c39eb49
wizjin <wizjin@icloud.com>
2023-04-09 10:23:19 +0800

    feat: remove unused options.

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,9 @@
 -- NeoVim Options
 -- General
-vim.o.history = 5000
-vim.o.autoread = true
 vim.o.showmode = false
 -- Search
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.hlsearch = true
-vim.o.incsearch = true
 vim.o.lazyredraw = false
 vim.o.magic = true
 vim.o.showmatch = true
@@ -17,14 +13,11 @@ vim.o.writebackup = false
 vim.o.swapfile = false
 vim.o.number = true
 vim.o.wrap = true
-vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.fileformats = 'unix,dos,mac'
 -- Text, tab and indent related
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
-vim.o.smarttab = true
 -- Tabs
-vim.o.tabpagemax = 10
 vim.o.showtabline = 2
 

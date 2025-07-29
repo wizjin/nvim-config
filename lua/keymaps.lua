2c5bb112f584f905ca1a28f04c263457fc272550
wizjin <wizjin@icloud.com>
2025-07-29 15:02:49 +0800

    Update which-key map.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,51 +7,38 @@ local telescope = require('telescope.builtin')
 -- Keymaps
 local wk = require("which-key")
 -- Global
-wk.register({
-	['<f9>'] = { '<cmd>:tabprevious<cr>', 'Goto prev tab' },
-	['<f10>'] = { '<cmd>:tabnext<cr>', 'Goto next tab' }
+wk.add({
+	{ "<f9>", "<cmd>:tabprevious<cr>", desc = "Goto prev tab" },
+	{ "<f10>", "<cmd>:tabnext<cr>", desc = "Goto next tab" }
 })
 -- Leader
-wk.register({
-	f = {
-		name = 'File',
-		f = { telescope.find_files, 'Find file' },
-		r = { telescope.oldfiles, 'Open recent file' },
-		b = { telescope.buffers, 'Find buffer' },
-		g = { telescope.live_grep, 'Live grep' },
-		w = { '<cmd>:write<cr>', 'Save' },
-		n = { '<cmd>:tabnew<cr>', 'New' }
-	},
-	p = {
-		name = 'Project',
-		p = { '<cmd>:NvimTreeFocus<cr>', 'Go to tree' },
-		t = { '<cmd>:NvimTreeToggle<cr>', 'Toggle' },
-		f = { '<cmd>:NvimTreeFindFile<cr>', 'Find file' }
-	},
-	w = {
-		name = 'Window',
-		s = { '<cmd>:vsplit<cr>', 'Split vertical' },
-		S = { '<cmd>:split<cr>', 'Split horizontal' },
-		n = { '<cmd>:vnew<cr>', 'New vertical' },
-		N = { '<cmd>:new<cr>', 'New horizontal' },
-		c = { '<cmd>:quit<cr>', 'Close' },
-		k = { '<cmd>:wincmd k<cr>', 'Go to up' },
-		j = { '<cmd>:wincmd j<cr>', 'Go to down' },
-		h = { '<cmd>:wincmd h<cr>', 'Go to left' },
-		l = { '<cmd>:wincmd l<cr>', 'Go to right' },
-		w = { '<cmd>:wincmd w<cr>', 'Go to next' },
-		W = { '<cmd>:wincmd W<cr>', 'Go to previous' }
-	},
-	u = {
-		name = 'Utils',
-		j = { '<cmd>:%!jq .<cr>', 'Json formatter' }
-	},
-	h = {
-		name = 'Help',
-		h = { '<cmd>:help<cr>', 'NeoVim' },
-		k = { '<cmd>:WhichKey<cr>', 'Keymaps' }
-	}
-}, {
-	prefix='<leader>'
+wk.add({
+	{ "<leader>f", group = "File" },
+	{ "<leadser>ff", telescope.find_files, desc = "Find file" },
+	{ "<leader>fr", telescope.oldfiles, desc = "Open recent file" },
+	{ "<leader>fb", telescope.buffers, desc = "Find buffer" },
+	{ "<leader>fg", telescope.live_grep, desc = "Live grep" },
+	{ "<leader>fw", "<cmd>:write<cr>", desc = "Save" },
+	{ "<leader>fn", "<cmd>:tabnew<cr>", desc = "New" },
+	{ "<leader>p", group = "Project" },
+	{ "<leader>pp",	"<cmd>:NvimTreeFocus<cr>", desc = "Go to tree" },
+	{ "<leader>pt", "<cmd>:NvimTreeToggle<cr>", desc = "Toggle" },
+	{ "<leader>pf", "<cmd>:NvimTreeFindFile<cr>", desc = "Find file" },
+	{ "<leader>w", group = "Window" },
+	{ "<leader>ws", "<cmd>:vsplit<cr>", desc = "Split vertical" },
+	{ "<leader>wS", "<cmd>:split<cr>", desc = "Split horizontal" },
+	{ "<leader>wn", "<cmd>:vnew<cr>", desc = "New vertical" },
+	{ "<leader>wN", "<cmd>:new<cr>", desc = "New horizontal" },
+	{ "<leader>wc", "<cmd>:quit<cr>", desc = "Close" },
+	{ "<leader>wk", "<cmd>:wincmd k<cr>", desc = "Go to up" },
+	{ "<leader>wj", "<cmd>:wincmd j<cr>", desc = "Go to down" },
+	{ "<leader>wh", "<cmd>:wincmd h<cr>", desc = "Go to left" },
+	{ "<leader>wl", "<cmd>:wincmd l<cr>", desc = "Go to right" },
+	{ "<leader>ww", "<cmd>:wincmd w<cr>", desc = "Go to next" },
+	{ "<leader>wW", "<cmd>:wincmd W<cr>", desc = "Go to previous" },
+	{ "<leader>u", group = "Utils" },
+	{ "<leader>uj", "<cmd>:%!jq .<cr>", desc = "Json formatter" },
+	{ "<leader>h", desc = "Help" },
+	{ "<leader>hh", "<cmd>:help<cr>", desc = "NeoVim" },
+	{ "<leader>hk", "<cmd>:WhichKey<cr>", desc = "Keymaps" }
 })
-
